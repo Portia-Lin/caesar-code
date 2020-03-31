6e8a024f8c1d78086a1e04c78a924f069409deff
Portia-Lin <mckaypeg@gmail.com>
2020-03-31 03:33:30 +0300

    polibius square

---
+++ PolybiusSquare/polybiussquare_encrypt.py
@@ -0,0 +1,28 @@
+matrix = [['M', 'A', 'T', 'R', 'I'],
+          ['X', 'B', 'C', 'D', 'E'],
+          ['F', 'G', 'H', 'K', 'L'],
+          ['N', 'O', 'P', 'Q', 'S'],
+          ['U', 'V', 'W', 'Y', 'Z']]
+
+matrixHeight = len(matrix)
+matrixWidth = len(matrix[0])
+
+def getCryptoChar(char):
+    for indexHeight in range(0, matrixHeight):
+        for indexWidth in range(0, matrixWidth):
+            if char == matrix[indexHeight][indexWidth]:
+                return matrix[(indexHeight + 1) % matrixHeight][indexWidth]
+    return char
+    pass
+
+def encryption(message):
+    newMessage = ""
+    for messageIndex in range(0, len(message)):
+        newMessage += getCryptoChar(message[messageIndex])
+    return newMessage
+
+print("Start!")
+message = input("Повідомлення: ").upper()
+result = encryption(message)
+print("Зашифроване повідомлення:")
+print(result)
