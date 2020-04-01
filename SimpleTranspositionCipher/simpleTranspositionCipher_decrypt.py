bfb0d39023d2494f53dbccda99dffb7f5c4eeefd
Portia-Lin <mckaypeg@gmail.com>
2020-04-02 01:29:04 +0300

    Simple Transposition Cipher decrypt function

--- SimpleTranspositionCipher/simpleTranspositionCipher_decrypt.py
+++ SimpleTranspositionCipher/simpleTranspositionCipher_decrypt.py
@@ -1,31 +1,27 @@
-import random
-
-def encrypt(msg):
+def decrypt(msg):
     i = 0
     temp = ""
     temp_list = []
     d = {}
-    while len(msg) - key_length < 0:
-        msg += chr(random.randrange(ord("А"), ord("Я")))
     while i < key_length:
         temp_list.append(int(permutation[i]) - 1)
-        d[temp_list[i]] = msg[i]
+        d[temp_list[i]] = msg[int(permutation[i]) - 1]
         i += 1
-    for key in sorted(d.keys()):
-        temp += d[key]
+    for key in d.values():
+        temp += key
     return temp
 
 def slicer(message):
     result = ""
     temp = message.replace(" ", "")
-    temp_list = [encrypt(temp[i:i + key_length]) for i in range(0, len(temp), key_length)]
+    temp_list = [decrypt(temp[i:i + key_length]) for i in range(0, len(temp), key_length)]
     for i in range(len(temp_list)):
         result += temp_list[i]
     return result
 
 print("Start!")
-message = input("Повідомлення: ").upper()
+message = input("Повідомлення: ")
 key_length = int(input("Введіть довжину ключа: "))
 permutation = input("Введіть ключ: ")
-print("Зашифроване повідомлення: ")
+print("Розшифроване повідомлення: ")
 print(slicer(message))
